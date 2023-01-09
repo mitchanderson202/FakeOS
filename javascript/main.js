32a1afe1c59249874e290dbc9bb1445fef528ec2
Mitchell Anderson <mitchellanderson202@hotmail.com>
2023-01-09 17:21:25 +1100

    updated window modals

--- javascript/main.js
+++ javascript/main.js
@@ -11,15 +11,15 @@ let timeString = date.toLocaleString("en-AU", {
 });
 
 document.getElementById(
-  "c-footer__time"
+  "footer__time"
 ).innerText = `${timeString.toUpperCase()}`;
 
-document.getElementById("c-footer__date").innerText = `${day}/${month}/${year}`;
+document.getElementById("footer__date").innerText = `${day}/${month}/${year}`;
 
 // Windows Menu
 
-let windowsMenu = document.getElementById("c-footer__menu__icon");
-let menu = document.getElementById("c-windows__menu");
+let windowsMenu = document.getElementById("footer__menu__icon");
+let menu = document.getElementById("windows__menu");
 let body = document.querySelector("body");
 
 windowsMenu.addEventListener("click", () => {
@@ -28,4 +28,88 @@ windowsMenu.addEventListener("click", () => {
   } else {
     menu.style.display = "block";
   }
+});
+
+// Desktop Items
+
+let img1 = document.getElementById("bin-img");
+let img2 = document.getElementById("steam-img");
+let img3 = document.getElementById("vscode-img");
+let modal1 = document.querySelector(".modal1");
+let modal2 = document.querySelector(".modal2");
+let modal3 = document.querySelector(".modal3");
+let closeModal1 = document.getElementById("close-btn1");
+let closeModal2 = document.getElementById("close-btn2");
+let closeModal3 = document.getElementById("close-btn3");
+
+img1.addEventListener("click", () => {
+  img1.style.border = "1px dotted grey";
+  img2.style.border = "";
+  img3.style.border = "";
+
+  img1.style.background = "rgb(112, 112, 112)";
+  img2.style.background = "";
+  img3.style.background = "";
+});
+
+img2.addEventListener("click", () => {
+  img2.style.border = "1px dotted grey";
+  img1.style.border = "";
+  img3.style.border = "";
+
+  img2.style.background = "rgb(112, 112, 112)";
+  img1.style.background = "";
+  img3.style.background = "";
+});
+
+img3.addEventListener("click", () => {
+  img3.style.border = "1px dotted grey";
+  img1.style.border = "";
+  img2.style.border = "";
+
+  img3.style.background = "rgb(112, 112, 112)";
+  img1.style.background = "";
+  img2.style.background = "";
+});
+
+img1.addEventListener("dblclick", () => {
+  modal1.style.display = "block";
+  img2.style.border = "";
+  img1.style.border = "";
+  img3.style.border = "";
+  img2.style.background = "";
+  img1.style.background = "";
+  img3.style.background = "";
+});
+
+closeModal1.addEventListener("click", () => {
+  modal1.style.display = "none";
+});
+
+closeModal2.addEventListener("click", () => {
+  modal2.style.display = "none";
+});
+
+closeModal3.addEventListener("click", () => {
+  modal3.style.display = "none";
+});
+
+img2.addEventListener("dblclick", () => {
+  modal2.style.display = "block";
+  img2.style.border = "";
+  img1.style.border = "";
+  img3.style.border = "";
+  img2.style.background = "";
+  img1.style.background = "";
+  img3.style.background = "";
+});
+
+img3.addEventListener("dblclick", () => {
+  modal3.style.display = "block";
+  img2.style.border = "";
+  img1.style.border = "";
+  img3.style.border = "";
+  img2.style.background = "";
+  img1.style.background = "";
+  img3.style.background = "";
 });
